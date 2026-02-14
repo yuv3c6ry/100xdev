6a9b769f88645566ed60fa2005290e4646a5db23
Yuvraj Choudhary <yuvrajchoudhary04@gmail.com>
2026-02-14 13:40:39 +0530

    anagarm.js

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,16 @@
 */
 
 function isAnagram(str1, str2) {
- 
-}
+   const str = str1.toLowerCase().split('').sort().join('');
+   const strr = str2.toLowerCase().split('').sort().join('');
+   console.log(str1.split().sort())
+   if(str === strr){
+    return true
+   }else{
+    return false
+   }
+};
+isAnagram("man" , "amn")
+
 
 module.exports = isAnagram;
